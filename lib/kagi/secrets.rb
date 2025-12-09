31c7548a27f5c7f13da71d59098c267cb65f462b
jacoyutorius <jacoyutorius@gmail.com>
2025-12-09 16:29:32 +0900

    ✨ 環境変数の認証情報を優先的に使用するように改善
    - AWS_ACCESS_KEY_ID と AWS_SECRET_ACCESS_KEY が設定されている場合は環境変数を優先
    - AWS_SESSION_TOKEN にも対応
    - 一時的な認証情報（Session Token付き）が使えるようになった

--- lib/kagi/secrets.rb
+++ lib/kagi/secrets.rb
@@ -29,11 +29,15 @@ module Kagi
 
     # AWS クライアントを作成する
     def create_client(profile:, region:)
-      credentials = if profile == "default"
-        # default の場合は明示的に credentials を指定しない
-        # (環境変数や IAM Role を優先)
+      # 環境変数が設定されている場合は優先的に使用
+      credentials = if ENV['AWS_ACCESS_KEY_ID'] && ENV['AWS_SECRET_ACCESS_KEY']
+        # 環境変数から認証情報を取得（Session Token にも対応）
+        nil  # credentials を nil にすると AWS SDK が環境変数を自動的に使用
+      elsif profile == "default"
+        # default の場合も環境変数や IAM Role を優先
         nil
       else
+        # 指定されたプロファイルを使用
         Aws::SharedCredentials.new(profile_name: profile)
       end
 
